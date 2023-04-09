620f1539e30c468e0759b4877abf0a4c2f7a9c87
Bhuvanesh <bhuvanesh13997@gmail.com>
2023-04-09 20:12:03 +0530

    code optimizing & corrections

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,9 +1,6 @@
 import React, { useState,useEffect } from 'react';
-import {Card,Form,Button} from 'react-bootstrap';
+import {Form,Button} from 'react-bootstrap';
 import { Link,useNavigate } from "react-router-dom";
-import brand_logo from './../../assets/brand_logo.png';
-import illustration_openhead from './../../assets/illustration_openhead.png';
-import illustration_tape from './../../assets/illustration_tape.png';
 
 function LoginForm() {
 
@@ -56,39 +53,31 @@ function LoginForm() {
         console.log("formValues",formValues);
 
         navigate("/login/success");
+
+        setTimeout(()=>{
+            alert("logged In")
+        },3000)
     }
   },[formErrors])
 
   return (
     <>
-        <Card className='LoginForm'>
-            <Card.Body>
-                <img src={illustration_tape} className='illustration_tape img-fluid' alt='illustration_tape'/>
-                <img src={brand_logo} alt='brand_logo' className='brand_logo'/>
-                <h2 className='heading'>Please login to continue</h2>
-
-                <Form onSubmit={SubmitHandler}>
-                    <Form.Group className="mb-3" controlId="formGroupEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="text" placeholder="Enter email" name='email' onChange={(event)=>ChangeHandler(event)} autoComplete='off'/>
-                        <span className='error_msg'>{formErrors.email}</span>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formGroupPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Enter password" name='password' onChange={(event)=>ChangeHandler(event)} autoComplete='off'/>
-                        <span className='error_msg'>{formErrors.password}</span>
-                    </Form.Group>
-                    <div className='forgot_password'><Link to='/forgot_password'>Forgot Password?</Link></div>
-                    <Button type="submit" className='btn_continue'>Continue</Button>
-                </Form>
-                
-                <div className='sign_up'>Don't have an account? <Link to='/forgot_password'>Sign Up</Link></div>
-            </Card.Body>
-            <Card.Footer>
-                <img src={illustration_openhead} className='illustration_openhead img-fluid' alt='illustration_openhead'/>
-            </Card.Footer>
-        </Card>
-         
+      <Form onSubmit={SubmitHandler} className='LoginForm'>
+          <h2 className='heading'>Please login to continue</h2>
+          <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="text" placeholder="Enter email" name='email' onChange={(event)=>ChangeHandler(event)} autoComplete='off'/>
+              <span className='error_msg'>{formErrors.email}</span>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formGroupPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Enter password" name='password' onChange={(event)=>ChangeHandler(event)} autoComplete='off'/>
+              <span className='error_msg'>{formErrors.password}</span>
+          </Form.Group>
+          <div className='forgot_password'><Link to='/forgot_password' className='link'>Forgot Password?</Link></div>
+          <Button type="submit" className='btn_continue'>Continue</Button>
+          <div className='sign_up'>Don't have an account? <Link to='/sign_up' className='link'>Sign Up</Link></div>
+      </Form>
     </>
   )
 }
--- src/routing/LoginPage.js
+++ src/routing/LoginPage.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import {Routes,Route} from "react-router-dom";
+import {Card} from 'react-bootstrap';
 import LoginForm from '../components/login/LoginForm';
 import SuccessMsg from '../components/login/SuccessMsg';
 import PageNotFound from '../components/library/404/PageNotFound';
+import brand_logo_and_text from './../../src/assets/brand_logo_and_text.png';
+import illustration_openhead from './../../src/assets/illustration_openhead.png';
+import illustration_tape from './../../src/assets/illustration_tape.png';
 import '../components/login/LoginPage.css'
 
-
 function LoginPage() {
+
   return (
     <div className='LoginPage'>
         <div className='row content_row'>
             <div className='col-12 col-lg-6'>
-                <Routes>
-                    <Route path="/" element={<LoginForm/>} />
-                    <Route path="/login" element={<LoginForm/>} />
-                    <Route path="/login/success" element={<SuccessMsg/>} />
-                    <Route path="*" element={<PageNotFound/>} />
-                </Routes>
+                <Card className='Login_Card'>
+                    <Card.Header>
+                        <img src={illustration_tape} alt='illustration_tape' className='illustration_tape img-fluid' />
+                        <img src={brand_logo_and_text} alt='brand_logo' className='brand_logo'/>
+                    </Card.Header>
+                    <Card.Body>
+                        {/* Only Form Content in Card-Body with change according to Routes(Ex: Login Form,Forgot Password Form, 
+                          Sign Up Form etc). But Card-Header(Logo) & Card-Footer(Illustraion) will remain common */}
+                        <Routes>
+                            <Route path="/" element={<LoginForm/>} />
+                            <Route path="/login" element={<LoginForm/>} />
+                            <Route path="/login/success" element={<SuccessMsg/>} />
+                            <Route path="*" element={<PageNotFound/>} />
+                        </Routes>
+                    </Card.Body>
+                    <Card.Footer>
+                        <img src={illustration_openhead} alt='illustration_openhead' className='illustration_openhead img-fluid' />
+                    </Card.Footer>
+                </Card>
             </div>
         </div>
     </div>
